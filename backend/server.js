f8204c372f3254cfbf37d9b8b657d3659192d3b8
Eduardo Bárez <edumousus@gmail.com>
2025-01-14 16:32:59 +0100

    Mejora de estabilidad y conexion con Mongodb en server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ app.use(express.json()); // Reemplazo de body-parser para analizar JSON
 // Conexión a MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Conexión a MongoDB establecida');
   } catch (err) {
     console.error('Error al conectar a MongoDB:', err);
